31b5579b23336e6b323c5369fa8d13b7ad59a3ed
admodev <admosdigital@yahoo.com>
2021-02-03 13:19:27 -0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import CanvasJSReact from "./lib/canvasjs.react";
 
 function App() {
+  var CanvasJS = CanvasJSReact.CanvasJS;
+  var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+  const options = {
+    title: {
+      text: "Sales",
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: [
+          { label: "January", y: 30 },
+          { label: "February", y: 10 },
+          { label: "March", y: 25 },
+          { label: "April", y: 20 },
+          { label: "May", y: 38 },
+        ],
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Store Control</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Dashboard</Nav.Link>
+          <Nav.Link href="#features">Employees</Nav.Link>
+          <Nav.Link href="#pricing">Logout</Nav.Link>
+          <Navbar.Text className="ml-5">
+            Signed in as: <a href="#profile">Employee One</a>
+          </Navbar.Text>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+
+      <div className="Chart">
+        <CanvasJSChart options={options} />
+      </div>
     </div>
   );
 }
